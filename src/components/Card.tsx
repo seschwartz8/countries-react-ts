342b73bbf3985b349e6677fd14e15134a0c9b9bf
Sarah Schwartz <seschwartz8@gmail.com>
2020-04-23 14:21:04 -0700

    allow card front/back

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import ModeContext from '../contexts/mode';
 
@@ -18,22 +18,33 @@ const Card: React.FC<CardProps> = ({
   capital,
 }: CardProps) => {
   const mode = useContext(ModeContext);
+  const [flipped, setFlipped] = useState(false);
 
   return (
-    <CardContainer mode={mode}>
-      <img src={imgUrl} alt={name}></img>
-      <div>
-        <h3>{name}</h3>
-        <p>
-          <b>Population: </b> {population}
-        </p>
-        <p>
-          <b>Region: </b> {region}
-        </p>
-        <p>
-          <b>Capital: </b> {capital}
-        </p>
-      </div>
+    <CardContainer
+      mode={mode}
+      flipped={flipped}
+      onClick={() => setFlipped((flipped) => !flipped)}
+    >
+      <CardBack>
+        <div>BACK</div>
+      </CardBack>
+
+      <CardFront>
+        <img src={imgUrl} alt={name}></img>
+        <div>
+          <h3>{name}</h3>
+          <p>
+            <b>Population: </b> {population}
+          </p>
+          <p>
+            <b>Region: </b> {region}
+          </p>
+          <p>
+            <b>Capital: </b> {capital}
+          </p>
+        </div>
+      </CardFront>
     </CardContainer>
   );
 };
@@ -44,31 +55,60 @@ export default Card;
 const CardContainer = styled.div`
   border-radius: 5px;
   margin: 2%;
-  max-width: 250px;
+  width: 250px;
 
+  ${({ mode = 'light' }: CardContainerProps) =>
+    mode === `light`
+      ? `
+    background-color: white;
+    color: black;
+    box-shadow: 0 0 8px #d9d9d9;
+    `
+      : `
+    background-color: #324354;
+    color: white;
+    box-shadow: 0 0 8px #1D2A36;
+`};
+
+  ${({ flipped = true }: CardContainerProps) =>
+    flipped === false
+      ? `
+      & ${CardFront} {
+        display: block;
+      }
+      & ${CardBack} {
+        display: none;
+      }
+      `
+      : `
+      & ${CardBack} {
+        display: block;
+      }
+      & ${CardFront} {
+        display: none;
+      }
+  `};
+`;
+
+interface CardContainerProps {
+  mode: string;
+  flipped: boolean;
+}
+
+const CardFront = styled.div`
   & div {
     margin: 5% 0;
     padding: 10%;
   }
 
   & img {
-    max-width: 250px;
+    max-width: 100%;
   }
-
-  ${({ mode = 'light' }: CardContainerProps) =>
-    mode === `light`
-      ? `
-      background-color: white;
-      color: black;
-      box-shadow: 0 0 8px #d9d9d9;
-      `
-      : `
-      background-color: #324354;
-      color: white;
-      box-shadow: 0 0 8px #1D2A36;
-      `};
 `;
 
-interface CardContainerProps {
-  mode: string;
-}
+const CardBack = styled.div`
+  & div {
+    margin: 5% 0;
+    padding: 10%;
+  }
+`;
